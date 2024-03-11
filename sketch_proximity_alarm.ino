1a5a23ddd0328a1e8fa833ad782e938fc960cf3e
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2024-03-11 00:07:40 -0300

    first commit.

---
+++ sketch_proximity_alarm.ino
@@ -0,0 +1,20 @@
+#include <HCSR04.h>
+
+const byte TRIGGER_PIN = 12;
+const byte ECHO_PIN = 13;
+
+UltraSonicDistanceSensor distance_sensor(TRIGGER_PIN, ECHO_PIN);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float distance = distance_sensor.measureDistanceCm();
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println("cm");
+
+  delay(100);
+}
